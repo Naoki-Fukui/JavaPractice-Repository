03c9ed2ca95f2c3d1340693156fa5b2d5a16ede9
aokcat715 <fn.driveline@gmail.com>
2025-08-15 15:11:17 +0900

    ラムダ式の記述方法とStreamAPI .map().forEach()の練習 #3

---
+++ src/com/practicejava/main/learnlamda/lamdainterface/FunctionInterface.java
@@ -0,0 +1,9 @@
+package com.practicejava.main.learnlamda.lamdainterface;
+
+@FunctionalInterface
+public interface FunctionInterface {
+	
+	// 引数ありの抽象メソッドを1つ定義
+	public String greet(String name);
+
+}
